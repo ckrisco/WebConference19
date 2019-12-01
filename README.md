e317bac0428bf1aaa34a98692b22af2e10364453
ckrisco <travelgreenlight@gmail.com>
2019-12-01 10:47:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-t9zfqt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-t9zfqt)
---
+++ dateApp/src/lab.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render (
+  new Date().toString(),
+  document.getElementById('contents'));
+
+
